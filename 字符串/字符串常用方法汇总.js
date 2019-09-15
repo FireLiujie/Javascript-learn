13999be1a975c49851d3b3af225faf427c56ee8c
FireLiujie <445198516@qq.com>
2019-09-15 14:07:05 +0800

    [NEW] 新增字符串截取类方法

--- 字符串/字符串常用方法汇总.js
+++ 字符串/字符串常用方法汇总.js
@@ -1,3 +1,5 @@
+//1、查找类方法
+
 /**
  * 获取字符串的第n位
  * str.charAt(n)
@@ -16,3 +18,26 @@ console.log(str.indexOf('sd')) // 4
  * str.lastIndexOf(subStr,index)
  */
 console.log(str.lastIndexOf('sd')) // 14
+
+
+// 2、截取类方法
+
+/**
+ * slice(start,end) (slice严格按照参数的顺序，第0位start，第1位end)
+ * substr(start,length)
+ * substring(start,end) (substring会比较start和end谁小从谁start)
+ * 
+ * start 从哪位开始截取(只写start的话，就从start开始一直截取到最后)
+ * end 截取到第几位(注意不包含end位)
+ * length 截取几位
+ * 
+ * 字符串中，所有的截取类方法，都是返回一个新的字符串，并不会对原来的字符串进行操作
+ */
+let a = 1
+let b = 2
+console.log(str.slice(a,b)) // 2
+console.log(str.slice(b,a)) // ''
+console.log(str.substring(a,b)) // 2
+console.log(str.substring(b,a)) // 2
+console.log(str.substring(a,b)) // 2
+console.log(str.substring(b,a)) // 2
