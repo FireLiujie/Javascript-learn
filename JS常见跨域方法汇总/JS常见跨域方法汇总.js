c54e8251262b8388dd932c329ffc816493fde5f6
Fireliujie <445198516@qq.com>
2019-09-25 08:43:22 +0800

    [NEW] 新增跨域方法及Linux命令大全

--- JS常见跨域方法汇总/JS常见跨域方法汇总.js
+++ JS常见跨域方法汇总/JS常见跨域方法汇总.js
@@ -397,3 +397,61 @@ server.listen(4000, () => {
  * 上述代码进过两次跨域，值得注意的是浏览器向代理服务器发送请求，也遵循同源策略，最后在
  * index.html文件打印出{'title':'fontend','password': '123456'}
  */
+
+/**
+ * 6、nginx反向代理
+ * 实现原理类似于Node中间件代理，需要你搭建一个中转nginx服务器，用于转发请求。
+ *
+ * 使用nginx反向代理实现跨域，是最简单的跨域方式。只需要修改nginx的配置即可解决跨域问题，
+ * 支持所有浏览器，支持session，不需要修改任何代码，并且不会影响服务器性能。
+ *
+ * 实现思路:通过nginx配置一个代理服务器（域名与domain1相同，端口不同）做跳板机，反向代理访问
+ * domain2接口，并且可以顺便修改cookie中domain信息，方便当前域cookie写入，实现跨域登录
+ *
+ * 先下载nginx，然后将nginx目录下的nginx.conf修改如下：
+ */
+
+// proxy 服务器
+/**
+ *
+ server {
+  listen 81;
+  server_name www.domain1.com
+  location / {
+    proxy_pass http://www.domain2.com:8080; #反向代理
+    proxy_cookie_domain www.domain2.com www.domain1.com #修改cookie里域名
+    index index.html index.htm
+
+    #当用webpack-dev-server等中间件代理接口访问nginx时，此时无浏览器参与，故没有同源限制
+    下面的跨域配置可不启用
+    add_header Access-Control-Allow-Origin http://www.domain1.com #当前端只跨域不带cookie时，可为*
+    add_header Access-Control-Allow-Credentials true;
+  }
+}
+
+最后通过命令行 nginx -s reload启动nginx
+ */
+
+// index.html
+var xhr = new XMLHttpRequest();
+// 前端开关：浏览器是否读写cookie
+xhr.withCredentials = true;
+// 访问nginx中的代理服务器
+xhr.open("get", "http://www.domain1.com:81/?user=admin", true);
+xhr.send();
+
+// server.js
+var http = require("http");
+var server = http.createServer();
+var qs = require("querystring");
+server.on("request", function(req, res) {
+  var params = qs.parse(req.url.substring(2));
+  // 向前台写cookie
+  res.writeHead(200, {
+    "Set-Cookie": "l=a123456;Path=/;Domain=www.domain2.com;HttpOnly" // HttpOnly:脚本无法读取
+  });
+  res.write(JSON.stringify(params));
+  res.end();
+});
+server.listen("8080");
+console.log("Server is running at port 8080...");
