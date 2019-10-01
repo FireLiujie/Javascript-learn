eae2ebeb1441083b4ba88756452e1db0b5942da1
FireLiujie <445198516@qq.com>
2019-10-01 23:31:43 +0800

    [NEW] 新增正则search用法

--- 正则表达式/正则表达式.js
+++ 正则表达式/正则表达式.js
@@ -7,3 +7,12 @@
  var str = 'fyguhijfuh'
  var re = /a/
  console.log(re.test(str)) // false
+
+ /**
+  * search
+  * search方法用来查找字符串中是否含有正则规定的内容，如果有则返回索引，否则返回-1
+  * 语法：字符串.search(正则)
+  */
+ var str = 'tuiosfausfi'
+ var re = /ab/
+ console.log(str.search(re)) // -1
