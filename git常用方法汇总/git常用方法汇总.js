b6c64b2295ef16f7cf71ffe2153dd3de9ff038cc
Fireliujie <445198516@qq.com>
2019-10-24 09:21:57 +0800

    [NEW] git分支操作

--- git常用方法汇总/git常用方法汇总.js
+++ git常用方法汇总/git常用方法汇总.js
@@ -1,5 +1,21 @@
 /**
+ * 使用git pull 从远程仓库“同步”代码，通常有三种方式；
  * git pull origin <remote_branch>:<local_branch>
- * 场景一：当前分支是dev,但是你想把远程master同步到本地master，但又不想使用checkout切换到master分支，这是就可以使用：
+ * git pull origin <remote_branch>
+ * git pull
+ */
+
+/**
+ * git pull origin <remote_branch>:<local_branch>
+ * 场景：当本地的当前分支不是local_branch
+ * 作用：将远程分支拉取到指定本地分支
+ * 例子：当前分支是dev,但是你想把远程master同步到本地master，但又不想使用checkout切换到master分支，这是就可以使用：
  * git pull origin master:master
  */
+
+ /**
+  * git pull origin <remote_branch>
+  * 场景：在当前分支上进行同步操作
+  * 作用：将指定远程分支同步到当前本地分支
+  * git pull origin master
+  */
