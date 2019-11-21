cedba9d417bc22295fae6173c4adf015d650ca86
刘杰 <445198516@qq.com>
2019-11-21 17:16:13 +0800

    [FIX] mac结束占用端口的所有进程

--- mac下查看端口是否被占用/mac下查看端口是否被占用.js
+++ mac下查看端口是否被占用/mac下查看端口是否被占用.js
@@ -2,7 +2,10 @@
  * 方法一：
  * 查看80端口是否被占用
  * sudo lsof -i :80
- * 
+ *
  * 方法二：
  * netstat -anp tcp | grep 80
+ *
+ * 如下命令可以直接结束占用端口的所有进程：
+ * lsof -P | grep ':80' | awk '{print $2}' | xargs kill -9
  */
