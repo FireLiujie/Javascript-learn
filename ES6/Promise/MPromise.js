561023200e0051e451abf1a6ebe24bcaf0912a0f
刘杰 <445198516@qq.com>
2020-01-17 09:42:26 +0800

    [NEW] 新增mpromise

--- ES6/Promise/MPromise.js
+++ ES6/Promise/MPromise.js
@@ -1,3 +1,19 @@
 class MPromise {
-  constructor() {}
+  static PENDING = 'PENDING'
+  static RESOLVED = 'RESOLVED'
+  static REJECTED = 'REJECTED'
+  constructor(handle) {
+    if (typeof handle !== 'function') {
+      throw new TypeError(
+        'Promise resolver undefined is not a function at new Promise '
+      )
+    }
+    handle(this._resolve, this, this._reject)
+  }
+  _resolve() {
+    console.log('resolve')
+  }
+  _reject() {
+    console.log('reject')
+  }
 }
