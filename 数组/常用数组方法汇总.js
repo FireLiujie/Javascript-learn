ef4ef8d662ab806921aa7eff378534b7546d7d47
刘杰 <liu.jie@ebiz-fooge.com>
2021-03-30 14:38:23 +0800

    数字扁平化

--- 数组/常用数组方法汇总.js
+++ 数组/常用数组方法汇总.js
@@ -1,7 +1,6 @@
-// 去重
-
 let arr = [1,2,1,1,1,3,2,3]
 
+// 去重
 // 方法一：
 // let list = [...new Set(arr)]
 // console.log(list)
---
+++ 数组扁平化/index.js
@@ -0,0 +1,32 @@
+let arr = [1, [2], [[3], [4, [5]]], 6]
+let arr1 = [1,[2],[3]]
+
+function flat(arr) {
+    let list = []
+    arr.forEach(item => {
+        if (Array.isArray(item)) {
+            list = [...list,...flat(item)]
+        } else {
+            list.push(item)
+        }
+    })
+    return list
+}
+
+// let list = flat(arr)
+// console.log(list)
+
+function flatByDeep(arr, deep) {
+    let list = []
+    arr.forEach(item => {
+        if (Array.isArray(item) && deep >= 1) {
+            list = list.concat(flatByDeep(item,deep - 1))
+        } else {
+            list.push(item)
+        }
+    })
+    return list
+}
+
+let list1 = flatByDeep(arr,1)
+console.log(list1)
